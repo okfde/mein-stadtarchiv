84d8b04b1dc65ad9d6f6fd51e56c3790695b7d23
Ernesto Ruge <mail@ernestoruge.de>
2019-11-29 15:59:46 +0000

    auto update es when updating file

--- webapp/storage/File.py
+++ webapp/storage/File.py
@@ -10,8 +10,11 @@ Redistribution and use in source and binary forms, with or without modification,
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """
 
-from mongoengine import ReferenceField, DateTimeField, StringField, ListField, DecimalField, IntField, BooleanField, DictField
+
+from mongoengine import signals
+from mongoengine import ReferenceField, DateTimeField, StringField, IntField, BooleanField, DictField
 from .Base import Base
+
 
 
 class File(Base):
@@ -34,3 +37,11 @@ class File(Base):
 
     def __repr__(self):
         return '<File %r>' % self.name
+
+
+def update_index(sender, document):
+    from ..data_worker.DataWorkerHelper import worker_celery_single as index_document
+    index_document.apply_async((str(document.document.id), ), countdown=5)
+
+
+signals.pre_save.connect(update_index, sender=File)
